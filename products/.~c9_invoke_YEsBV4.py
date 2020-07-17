1f5ce725a2192b8295b2509abdf78ae3e6463cf5
Ubuntu <ubuntu@ip-172-31-34-97.ec2.internal>
2020-07-17 12:45:56 +0000

    added a cart file

--- products/.~c9_invoke_YEsBV4.py
+++ products/.~c9_invoke_YEsBV4.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from .views import all_products
 
 urlpatterns = [
-    url(r'^$', all_products, name='products' action='add_to_cart'),
+    url(r'^$', all_products, name='products' action={%url'add_to_cart' }),
 ]
