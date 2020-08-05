2460396873de2e660f358f1ae67724724d47d1e9
Ubuntu <ubuntu@ip-172-31-34-97.ec2.internal>
2020-08-05 10:49:12 +0000

    updated products.html file

--- cart/views.py
+++ cart/views.py
@@ -14,7 +14,7 @@ def add_to_cart(request, id):
     cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return render(request, "cart.html")
 
 
 def adjust_cart(request, id):
